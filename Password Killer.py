9f21c638d83e1954d77b04b79e67c3b236ff4e27
Sina.f <sina.programerr@gmail.com>
2021-09-30 22:46:02 -0600

    Update Password Killer.py

--- Password Killer.py
+++ Password Killer.py
@@ -2,21 +2,63 @@ from itertools import cycle
 from sys import stdout
 from time import sleep
 import subprocess
+import psutil
 import os
 
+users = [user.name for user in psutil.users()]
 loading_chars = iter(cycle('/-\|'))
-current_user = os.getlogin()
 os.system('cls')
 
+sleep(.5)
 print('''\n Welcome to Password Killer(written by Sina.f)\n''')
 sleep(1)
-submit = input(' Are you sure to remove the password for current user?(y/n) ').lower()
+
+for i in range(32):
+    sleep(0.15)
+    stdout.write(f'\r Extracting users... {next(loading_chars)}')
+
+print('\n')
+sleep(1)
+
+for idx, user in enumerate(users):
+    print(f' {idx+1}_ {user}')
+    sleep(1)
+    
+    
+selected_user = input('\n Select a user to kill(remove) the login password: ')
+
+while True:
+    sleep(.5)
+    try:
+        selected_user = int(selected_user)
+        assert selected_user <= len(users) and selected_user > 0
+        selected_user -= 1
+        break
+    
+    except AssertionError:
+        print(f'\n "{selected_user}" is not a valid user number!')
+        sleep(.5)
+        selected_user = input('\n Please enter a valid user: ')        
+        
+    except:
+        if selected_user in users:
+            selected_user = users.index(selected_user)
+            break
+            
+        else:
+            print(f'\n "{selected_user}" is not a valid user!')
+            sleep(.5)
+            selected_user = input('\n Please enter a valid user: ')
+                
+user = users[selected_user]
+sleep(.7)
+submit = input(f'\n Are you sure to remove the password for "{user}" (y/n)? ').lower()
 
 if submit == 'y':
     # cmd --> command prompt
-    cmd = subprocess.Popen(['net', 'user', current_user, '*'], shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, stdin=subprocess.PIPE)
-    cmd.stdin.write(''.encode())
-    cmd.stdin.write(''.encode())
+    cmd = subprocess.Popen(['net', 'user', user, '*'], shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, stdin=subprocess.PIPE)
+    cmd.stdin.write('\n'.encode())
+    cmd.stdin.write('\n'.encode())
     cmd.stdin.close()
     
     print()
@@ -32,21 +74,21 @@ if submit == 'y':
             stdout.write(f'\r Processing... {next(loading_chars)}')
             
         sleep(1)
-        print('\n\n Your password successfully removed!')
+        print(f'\n\n Password for "{user}" successfully killed(removed)!')
         sleep(.5)
-
-figlet =  f'''\n
+    
+figlet =  '''\n
    _____ _                __ 
   / ____(_)              / _|
  | (___  _ _ __   __ _  | |_ 
   \___ \| | '_ \ / _` | |  _|
   ____) | | | | | (_| |_| |  
  |_____/|_|_| |_|\__,_(_)_| 
-'''
+\n\n'''
 
 for line in figlet.splitlines():
     print(line)
     sleep(.2)
     
 sleep(1)
-input('\n\n Press enter to exit...')
+input(' Press enter to exit...')
